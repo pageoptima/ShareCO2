8afac5aca0e22b5bdb19dab5b2719c9920d320aa
Bikash Santra <santrabikash921@gmail.com>
2025-07-18 11:52:28 +0530

    Ride loading UI

--- app/(authenticated)/rideDetails/components/loadingRide.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-
-
-
-export default loadingRide;
