8a9d05f015905f1c7c247343acc51188d09844a9
pravat <pravats459@gmail.com>
2025-08-01 01:24:59 +0530

    notification testing

--- app/_components/providers/AblyPushRegistrar.tsx
+++ app/_components/providers/AblyPushRegistrar.tsx
@@ -7,7 +7,35 @@ export function AblyPushRegistrar() {
 
     const ably = useAbly();
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     if ( !ably ) return;
+
+    //     ably.push.activate(
+    //         async (deviceDetails) => {
+
+    //             // Now register this browser on your backend
+    //             await fetch("/api/notification/register", {
+    //                 method: "POST",
+    //                 headers: { "Content-Type": "application/json" },
+    //                 body: JSON.stringify({
+    //                     deviceId    : deviceDetails.id,
+    //                     platform    : deviceDetails.platform,
+    //                     formFactor  : deviceDetails.formFactor,
+    //                     pushRecipent: deviceDetails.push.recipient,
+    //                 }),
+    //             });
+                
+    //         },
+    //         (error) => {
+    //             console.error( `Push activation failed:`, error );
+    //         }
+    //     );
+    // }, [ ably ]);
+
+    // return null;
+
+    const handleAllow = () => {
+
         if ( !ably ) return;
 
         ably.push.activate(
@@ -30,7 +58,11 @@ export function AblyPushRegistrar() {
                 console.error( `Push activation failed:`, error );
             }
         );
-    }, [ ably ]);
+    }
 
-    return null;
+    return (
+        <h1 onClick={handleAllow}>
+            Allow Notification
+        </h1>
+    );
 }
