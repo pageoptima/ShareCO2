2f6dbe8e0b96213f6a3f124e7cfed7edfe45f9ee
pravat <pravats459@gmail.com>
2025-08-02 18:56:18 +0530

    Notification try to fixed

---
+++ app/api/notification/unregister/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@/lib/auth/auth';
+import ably from '@/services/ably';
+
+export async function POST(req: Request) {
+
+    // Auth check and get the user id.
+    const session = await auth();
+    if (!session?.user?.id) {
+        return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+    }
+
+    // Get the device information from request body
+    const { deviceId } = await req.json();
+
+    if ( !deviceId ) {
+        return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+    }
+
+    // Unegister the device
+    await ably.push.admin.deviceRegistrations.remove(deviceId);
+
+    return NextResponse.json({ success: true }, { status: 201 });
+}
