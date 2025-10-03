474ee02c9ee0f2876351c3419cc9d7ceac201db3
Bikash Santra <santrabikash921@gmail.com>
2025-10-03 20:09:41 +0530

    minor changes

--- app/api/ride/create.ts
+++ app/api/ride/create.ts
@@ -33,6 +33,7 @@ export default async function handler(request: Request) {
                     (error instanceof Error && error.message) ||
                     "Unknown error",
             },
+
             { status: 500 }
         );
     }
